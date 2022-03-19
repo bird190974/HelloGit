0e086842bfc945f3bb94870481f21cd139b4b363
SIARHEI LEBEDZEU <data7479@mail.ru>
2022-03-19 07:40:33 +0300

    add 2nd line

--- src/by/bird74/hellogit/HelloGit.java
+++ src/by/bird74/hellogit/HelloGit.java
@@ -3,5 +3,6 @@ package by.bird74.hellogit;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("Seconf string");
     }
 }
